671411f40619c101fddd42928f16f78e08ed7959
almir <almir@dzinovic.net>
2016-07-06 00:02:13 +0200

    Revert "- send mail to user only on cron job errors"
    This reverts commit 1b4eb458c6e804a3edf0d63386a80f196373124a.

--- auto-update-repo/update-repo.sh
+++ auto-update-repo/update-repo.sh
@@ -51,7 +51,7 @@ else
   if [[ -n $(echo "$@" | grep "\-\-write-crontab") ]]; then
     if [[ -z $(crontab -l | grep ${SCRIPTNAME}) ]]; then
       echo "Crontab entry is being created."
-      crontab -l | { cat; echo -e "# Check for webhook releases every five minutes\n*/5 * * * * ${SCRIPTPATH}/${SCRIPTNAME} --user ${GITHUB_USER} --password ${GITHUB_PASS} > /dev/null"; } | crontab -
+      crontab -l | { cat; echo -e "# Check for webhook releases every five minutes\n*/5 * * * * ${SCRIPTPATH}/${SCRIPTNAME} --user ${GITHUB_USER} --password ${GITHUB_PASS}"; } | crontab -
     else
       echo "Crontab entry already exists."
     fi
